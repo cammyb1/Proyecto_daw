127cad28ba9fdd44446933bc6d1c330c9d08951a
cammyb1 <seiya_al_@hotmail.com>
2018-05-08 10:05:42 +0200

    Major change

--- php/index.php
+++ php/index.php
@@ -13,96 +13,43 @@
   </head>
   <body>
     <div id="root">
-      <div class="bg"></div>
-      <div class="main-content">
-        <div class="info">
-          <div class="title">
-            <h1>Cultura</h1>
-          </div>
-          <div class="u-info">
-            Añadido por
-            <span class="user_name">Admin</span>
-            el dia
-            <a href="#">10/04/2018</a>
-            .
+      <header>
+        <div class="container">
+          <div class="logo">
+            <div class="title">
+              <!-- <img src="../resources/logo.png" alt="logo"> -->
+              <h1>Titulo</h1>
+            </div>
+            <span>La civilizacion más grande y poderosa de América.</span>
           </div>
         </div>
-        <section>
-          <article class="">
-              <h1 class="a-title">Article title</h1>
-              <div class="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </header>
+      <nav class="menu">
+        <div class="container">
+          <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Cultura</a></li>
+            <li><a href="">Idioma</a></li>
+            <li><a href="">Guias</a></li>
+          </ul>
+        </div>
+      </nav>
+      <section>
+        <div class="container">
+          <article>
+            <div class="upper">
+              <h1>titulo</h1>
+              <div>
+                <span>10:24PM</span>/<span>admin</span>/cultura/254
               </div>
-              <div class="">
-                <span>Vistas</span>
-                <span>3223</span>
-              </div>
+            </div>
+            <div class="lower">
+              <img src="" alt="previewimg">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat labore molestias provident iure ipsa odit sit, vel minima debitis, error nisi accusantium incidunt natus delectus, autem voluptatum quae aliquid quaerat.</p>
+            </div>
           </article>
-        </section>
-        <aside>
-          <div class="">
-            <div class="title">
-              <h1>Files</h1>
-            </div>
-            <ul>
-              <li><a href="#">archivo 1</a></li>
-              <li><a href="#">archivo 2</a></li>
-              <li><a href="#">archivo 3</a></li>
-              <li><a href="#">archivo 4</a></li>
-              <li><a href="#">archivo 5</a></li>
-            </ul>
-          </div>
-          <div class="">
-            <div class="title">
-              <h1>Categories</h1>
-            </div>
-            <ul>
-              <li><a href="#">archivo 1</a></li>
-              <li><a href="#">archivo 2</a></li>
-              <li><a href="#">archivo 3</a></li>
-              <li><a href="#">archivo 4</a></li>
-              <li><a href="#">archivo 5</a></li>
-            </ul>
-          </div>
-        </aside>
-        <div class="footer">
-          <p>Lorem ipsum do &copy reserver 2018</p>
         </div>
-      </div>
-      <div class="r-bar">
-        <div class="bookmark">
-          <div class="b-title">
-            <h1>Cultura</h1>
-          </div>
-          <div class="b-tumbnail">
-            <img src="" alt="tumbnail">
-          </div>
-        </div>
-        <div class="bookmark">
-          <div class="b-title">
-            <h1>Comunicacion</h1>
-          </div>
-          <div class="b-tumbnail">
-            <img src="" alt="tumbnail">
-          </div>
-        </div>
-        <div class="bookmark">
-          <div class="b-title">
-            <h1>Economia</h1>
-          </div>
-          <div class="b-tumbnail">
-            <img src="" alt="tumbnail">
-          </div>
-        </div>
-        <div class="close-bookmarks">
-          <a href="">></a>
-        </div>
-      </div>
+      </section>
     </div>
   </body>
 </html>
